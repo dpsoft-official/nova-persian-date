f6aa77201fccc146b8d881995fd548b291083ac3
SadeghPM <SadeghPM@users.noreply.github.com>
2023-07-26 12:48:42 +0330

    Update field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,9 +1,9 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('index-nova-persian-date', require('./components/IndexField'))
-    Vue.component('detail-nova-persian-date', require('./components/DetailField'))
-    Vue.component('form-nova-persian-date', require('./components/FormField'))
+    Vue.component('index-nova-persian-date', require('./components/IndexField').default)
+    Vue.component('detail-nova-persian-date', require('./components/DetailField').default)
+    Vue.component('form-nova-persian-date', require('./components/FormField').default)
 
-    Vue.component('index-nova-persian-date-time', require('./components/date-time/IndexField'))
-    Vue.component('detail-nova-persian-date-time', require('./components/date-time/DetailField'))
-    Vue.component('form-nova-persian-date-time', require('./components/date-time/FormField'))
+    Vue.component('index-nova-persian-date-time', require('./components/date-time/IndexField').default)
+    Vue.component('detail-nova-persian-date-time', require('./components/date-time/DetailField').default)
+    Vue.component('form-nova-persian-date-time', require('./components/date-time/FormField').default)
 })
